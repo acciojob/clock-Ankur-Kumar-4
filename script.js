22eea69399a9434ac3de6348f37c1b4fd2796a38
Ankur Kumar <98178905+Ankur-Kumar-4@users.noreply.github.com>
2023-09-20 09:39:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+
+function updateTimer() {
+	const timerElement = document.getElementById("timer");
+    const currentDate = new Date();
+	
+
+	const formattedDate = currentDate.toLocaleString();
+
+	timerElement.textContent =formattedDate;
+	
+}
+	   setInterval(updateTimer,1000);
+
+updateTimer();
+
